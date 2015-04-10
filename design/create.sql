ef2bd7d17139e157838105533a2a88df77ddfddd
redtreelchao <redtreelchao@126.com>
2015-04-10 18:25:46 +0800

    sql
    export sql from sqlyog

--- design/create.sql
+++ design/create.sql
@@ -1,185 +1,156 @@
-# ---------------------------------------------------------------------- #
-# Script generated with: DeZign for Databases V8.1.2                     #
-# Target DBMS:           MySQL 5                                         #
-# Project file:          tuanzhao.dez                                    #
-# Project name:                                                          #
-# Author:                                                                #
-# Script type:           Database creation script                        #
-# Created on:            2015-04-09 10:01                                #
-# ---------------------------------------------------------------------- #
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.41-0+wheezy1 : Database - tuanzhao
+*********************************************************************
+*/
 
+/*!40101 SET NAMES utf8 */;
 
-# ---------------------------------------------------------------------- #
-# Add tables                                                             #
-# ---------------------------------------------------------------------- #
+/*!40101 SET SQL_MODE=''*/;
 
-# ---------------------------------------------------------------------- #
-# Add table "rd_users"                                                   #
-# ---------------------------------------------------------------------- #
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`tuanzhao` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-CREATE TABLE `rd_users` (
-    `id` INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(32) NOT NULL COMMENT '”√ªß√˚',
-    `fullname` VARCHAR(20) COMMENT '’Ê µ–’√˚',
-    `add_time` INTEGER(11) COMMENT '◊¢≤· ±º‰',
-    `reg_ip` VARCHAR(15) COMMENT '◊¢≤·IP',
-    `id_num` VARCHAR(40) COMMENT '…Ì∑›÷§∫≈',
-    `money` INTEGER(8) COMMENT '«Æ',
-    `email` VARCHAR(64) COMMENT '” œ‰',
-    `mobile` INTEGER(11) COMMENT ' ÷ª˙∫≈',
-    `qq` VARCHAR(64) COMMENT 'QQ∫≈',
-    CONSTRAINT `PK_rd_users` PRIMARY KEY (`id`)
-);
+USE `tuanzhao`;
 
-# ---------------------------------------------------------------------- #
-# Add table "rd_user_stats"                                              #
-# ---------------------------------------------------------------------- #
+/*Table structure for table `rd_company` */
 
-CREATE TABLE `rd_user_stats` (
-    `id` INTEGER(11) NOT NULL AUTO_INCREMENT,
-    `user_id` INTEGER(11) NOT NULL,
-    `add_time` INTEGER(11) NOT NULL,
-    CONSTRAINT `PK_rd_user_stats` PRIMARY KEY (`id`, `user_id`)
-);
+CREATE TABLE `rd_company` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `user_num` mediumint(5) NOT NULL DEFAULT '0',
+  `desc` text NOT NULL,
+  `logo` varchar(128) DEFAULT NULL COMMENT 'logo',
+  `add_time` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# ---------------------------------------------------------------------- #
-# Add table "rd_companies"                                               #
-# ---------------------------------------------------------------------- #
-
-CREATE TABLE `rd_companies` (
-    `id` INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(128) NOT NULL,
-    `user_num` MEDIUMINT(5) NOT NULL DEFAULT 0,
-    `desc` TEXT NOT NULL DEFAULT '’‚ «ÃÓ–¥π´ÀæΩÈ…‹',
-    `logo` VARCHAR(128) COMMENT 'logo',
-    `add_time` INTEGER(11) NOT NULL,
-    CONSTRAINT `PK_rd_companies` PRIMARY KEY (`id`)
-);
-
-# ---------------------------------------------------------------------- #
-# Add table "rd_company_user"                                            #
-# ---------------------------------------------------------------------- #
+/*Table structure for table `rd_company_user` */
 
 CREATE TABLE `rd_company_user` (
-    `id` INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `user_id` INTEGER(11) NOT NULL,
-    `company_id` INTEGER(11) NOT NULL,
-    `add_time` INTEGER(11) NOT NULL,
-    CONSTRAINT `PK_rd_company_user` PRIMARY KEY (`id`)
-);
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `company_id` int(11) unsigned NOT NULL,
+  `add_time` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rd_companies_rd_company_user` (`company_id`),
+  KEY `rd_users_rd_company_user` (`user_id`),
+  CONSTRAINT `rd_users_rd_company_user` FOREIGN KEY (`user_id`) REFERENCES `rd_user` (`id`),
+  CONSTRAINT `rd_companies_rd_company_user` FOREIGN KEY (`company_id`) REFERENCES `rd_company` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# ---------------------------------------------------------------------- #
-# Add table "rd_industry"                                                #
-# ---------------------------------------------------------------------- #
-
-CREATE TABLE `rd_industry` (
-    `id` MEDIUMINT(7) NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(64) NOT NULL,
-    `parent_id` MEDIUMINT(7) NOT NULL DEFAULT 0,
-    `add_time` INTEGER(11) NOT NULL,
-    CONSTRAINT `PK_rd_industry` PRIMARY KEY (`id`)
-)
-––“µ∑÷¿‡;
-
-# ---------------------------------------------------------------------- #
-# Add table "rd_entity_type"                                             #
-# ---------------------------------------------------------------------- #
-
-CREATE TABLE `rd_entity_type` (
-    `id` SMALLINT(4) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` SMALLINT(4) NOT NULL,
-    `key` VARCHAR(10) NOT NULL,
-    `add_time` INTEGER(11) NOT NULL,
-    CONSTRAINT `PK_rd_entity_type` PRIMARY KEY (`id`)
-);
-
-# ---------------------------------------------------------------------- #
-# Add table "rd_entity_industry"                                         #
-# ---------------------------------------------------------------------- #
+/*Table structure for table `rd_entity_industry` */
 
 CREATE TABLE `rd_entity_industry` (
-    `id` INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `entity_key` CHAR(10) NOT NULL,
-    `industry_id` INTEGER(8) NOT NULL,
-    `add_time` INTEGER(11),
-    `entity_refer_id` INTEGER(11) COMMENT '”√ªßID°¢π´ÀæID°¢...',
-    CONSTRAINT `PK_rd_entity_industry` PRIMARY KEY (`id`)
-)
-º«¬ºπ´Àæ°¢Õ≈∂”À˘ Ù––“µ;
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `entity_key` char(10) NOT NULL,
+  `industry_id` mediumint(8) unsigned NOT NULL,
+  `add_time` int(11) DEFAULT NULL,
+  `entity_refer_id` int(11) DEFAULT NULL COMMENT 'Áî®Êà∑ID„ÄÅÂÖ¨Âè∏ID„ÄÅ...',
+  PRIMARY KEY (`id`),
+  KEY `rd_industry_rd_entity_industry` (`industry_id`),
+  CONSTRAINT `rd_industry_rd_entity_industry` FOREIGN KEY (`industry_id`) REFERENCES `rd_industry` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# ---------------------------------------------------------------------- #
-# Add table "rd_group"                                                   #
-# ---------------------------------------------------------------------- #
+/*Table structure for table `rd_entity_type` */
+
+CREATE TABLE `rd_entity_type` (
+  `id` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
+  `name` smallint(4) NOT NULL,
+  `key` char(10) NOT NULL,
+  `add_time` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `rd_group` */
 
 CREATE TABLE `rd_group` (
-    `id` INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `user_id` INTEGER(11) NOT NULL,
-    `name` VARCHAR(64) NOT NULL,
-    `desc` TEXT NOT NULL DEFAULT 'Õ≈∂”√Ë ˆ',
-    `add_time` INTEGER(11),
-    `user_num` SMALLINT(4),
-    CONSTRAINT `PK_rd_group` PRIMARY KEY (`id`)
-);
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `desc` text NOT NULL,
+  `add_time` int(11) DEFAULT NULL,
+  `user_num` smallint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# ---------------------------------------------------------------------- #
-# Add table "rd_jobs"                                                    #
-# ---------------------------------------------------------------------- #
+/*Table structure for table `rd_group_user` */
+
+CREATE TABLE `rd_group_user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `add_time` int(11) NOT NULL,
+  `job_id` mediumint(8) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rd_group_rd_group_users` (`group_id`),
+  KEY `rd_users_rd_group_users` (`user_id`),
+  CONSTRAINT `rd_users_rd_group_users` FOREIGN KEY (`user_id`) REFERENCES `rd_user` (`id`),
+  CONSTRAINT `rd_group_rd_group_users` FOREIGN KEY (`group_id`) REFERENCES `rd_group` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `rd_industry` */
+
+CREATE TABLE `rd_industry` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `parent_id` mediumint(7) NOT NULL DEFAULT '0',
+  `add_time` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `rd_jobs` */
 
 CREATE TABLE `rd_jobs` (
-    `id` INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(64) NOT NULL,
-    `add_time` INTEGER(11),
-    CONSTRAINT `PK_rd_jobs` PRIMARY KEY (`id`)
-);
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `add_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# ---------------------------------------------------------------------- #
-# Add table "rd_logs"                                                    #
-# ---------------------------------------------------------------------- #
+/*Table structure for table `rd_logs` */
 
 CREATE TABLE `rd_logs` (
-    `id` INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(12) NOT NULL,
-    `detail` TEXT,
-    `user_id` INTEGER(11) NOT NULL,
-    `add_time` INTEGER(11),
-    CONSTRAINT `PK_rd_logs` PRIMARY KEY (`id`)
-)
-º«¬º”√ªßÀ˘”–µƒ≤Ÿ◊˜°£;
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(12) NOT NULL,
+  `detail` text,
+  `user_id` int(11) NOT NULL,
+  `add_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# ---------------------------------------------------------------------- #
-# Add table "rd_group_users"                                             #
-# ---------------------------------------------------------------------- #
+/*Table structure for table `rd_user` */
 
-CREATE TABLE `rd_group_users` (
-    `id` INTEGER(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `group_id` INTEGER(11) NOT NULL,
-    `user_id` INTEGER(11) NOT NULL,
-    `add_time` INTEGER(11) NOT NULL,
-    `job_id` MEDIUMINT(8) NOT NULL,
-    CONSTRAINT `PK_rd_group_users` PRIMARY KEY (`id`)
-);
+CREATE TABLE `rd_user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL COMMENT 'Áî®Êà∑Âêç',
+  `fullname` varchar(20) DEFAULT NULL COMMENT 'ÁúüÂÆûÂßìÂêç',
+  `add_time` int(11) NOT NULL COMMENT 'Ê≥®ÂÜåÊó∂Èó¥',
+  `reg_ip` char(15) NOT NULL COMMENT 'Ê≥®ÂÜåIP',
+  `id_num` varchar(40) NOT NULL COMMENT 'Ë∫´‰ªΩËØÅÂè∑',
+  `money` int(8) DEFAULT '0' COMMENT 'Èí±',
+  `email` varchar(64) DEFAULT NULL COMMENT 'ÈÇÆÁÆ±',
+  `mobile` int(11) DEFAULT NULL COMMENT 'ÊâãÊú∫Âè∑',
+  `qq` varchar(64) DEFAULT NULL COMMENT 'QQÂè∑',
+  `password` char(32) NOT NULL COMMENT 'ÂØÜÁ†Å',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-# ---------------------------------------------------------------------- #
-# Add foreign key constraints                                            #
-# ---------------------------------------------------------------------- #
+/*Table structure for table `rd_user_stats` */
 
-ALTER TABLE `rd_user_stats` ADD CONSTRAINT `rd_users_rd_user_stats` 
-    FOREIGN KEY (`user_id`) REFERENCES `rd_users` (`id`);
+CREATE TABLE `rd_user_stats` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `add_time` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `rd_users_rd_user_stats` (`user_id`),
+  CONSTRAINT `rd_users_rd_user_stats` FOREIGN KEY (`user_id`) REFERENCES `rd_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `rd_company_user` ADD CONSTRAINT `rd_companies_rd_company_user` 
-    FOREIGN KEY (`company_id`) REFERENCES `rd_companies` (`id`);
-
-ALTER TABLE `rd_company_user` ADD CONSTRAINT `rd_users_rd_company_user` 
-    FOREIGN KEY (`user_id`) REFERENCES `rd_users` (`id`);
-
-ALTER TABLE `rd_entity_industry` ADD CONSTRAINT `rd_industry_rd_entity_industry` 
-    FOREIGN KEY (`industry_id`) REFERENCES `rd_industry` (`id`);
-
-ALTER TABLE `rd_entity_industry` ADD CONSTRAINT `rd_entity_type_rd_entity_industry` 
-    FOREIGN KEY (`entity_key`) REFERENCES `rd_entity_type` (`key`);
-
-ALTER TABLE `rd_group_users` ADD CONSTRAINT `rd_group_rd_group_users` 
-    FOREIGN KEY (`group_id`) REFERENCES `rd_group` (`id`);
-
-ALTER TABLE `rd_group_users` ADD CONSTRAINT `rd_users_rd_group_users` 
-    FOREIGN KEY (`user_id`) REFERENCES `rd_users` (`id`);
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
